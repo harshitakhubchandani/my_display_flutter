6d7f6e1c5f80a43c387ba09ce9964b1985dc28c2
RIL <harshita.kh0>
2021-10-07 21:41:09 +0530

    Display word

---
+++ display_name/lib/home_screen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class HomeScreen extends StatelessWidget {
+  HomeScreen({required this.value});
+  String value;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xffFFDADA),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(21.0),
+          child: Text(
+            'Hello '+value+', hope you are doing well...have a great day!',
+            textAlign: TextAlign.center,
+            style: GoogleFonts.lato(
+                textStyle: Theme.of(context).textTheme.headline4,
+                fontSize: 25,
+                fontWeight: FontWeight.w500,
+                fontStyle: FontStyle.italic,
+                color: Colors.black,
+                shadows: [
+                  Shadow(
+                    blurRadius: 10.0,
+                    color: Colors.grey,
+                    offset: Offset(5.0, 5.0),
+                  ),]),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
